0652f440fcd2bc568865e69c017e203fd103961a
hamid <hamid.nikoo@gmail.com>
2020-04-21 23:27:30 +0200

    Updates

---
+++ precache-manifest.26a42e09aae359a08a8f3bd084495a44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "907fd972e752007fed105f331b6d4fa2",
+    "url": "/coronaintera/index.html"
+  },
+  {
+    "revision": "f6e43832f32d5dca75fd",
+    "url": "/coronaintera/static/css/main.13c8a689.chunk.css"
+  },
+  {
+    "revision": "35c018d1ea5b8cb45509",
+    "url": "/coronaintera/static/js/2.276b8fea.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronaintera/static/js/2.276b8fea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6e43832f32d5dca75fd",
+    "url": "/coronaintera/static/js/main.073294db.chunk.js"
+  },
+  {
+    "revision": "7385a2f683921a3ae242",
+    "url": "/coronaintera/static/js/runtime-main.f54e6378.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronaintera/static/media/image.d7265326.png"
+  }
+]);
